db9e2f82e7240b45f6cace2779e2916de20a1402
Aliaksei Koran <akoran@scnsoft.com>
2022-06-10 02:15:34 +0300

    Add gateway endpoint

---
+++ gateway/src/main/java/com/bingo/gateway/handler/UserHandler.java
@@ -0,0 +1,30 @@
+package com.bingo.gateway.handler;
+
+import com.bingo.gateway.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Service
+public class UserHandler {
+
+    private final WebClient userWebClient;
+
+    public UserHandler(@Qualifier("userService") WebClient userWebClient) {
+        this.userWebClient = userWebClient;
+    }
+
+    public Mono<ServerResponse> login(ServerRequest serverRequest) {
+        Long userId = Long.parseLong(serverRequest.pathVariable("id"));
+        Mono<UserDto> dto = userWebClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/users/{id}").build(userId)).retrieve()
+                .bodyToMono(UserDto.class);
+        return dto.flatMap(s -> ServerResponse.ok().body(BodyInserters.fromValue(s)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+}
--- gateway/src/main/java/com/bingo/gateway/router/UserRouter.java
+++ gateway/src/main/java/com/bingo/gateway/router/UserRouter.java
@@ -1,14 +1,13 @@
-package com.bingo.userservice.router;
+package com.bingo.gateway.router;
 
-import com.bingo.userservice.handler.UserHandler;
+import com.bingo.gateway.handler.UserHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-
-import static org.springframework.http.MediaType.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
@@ -18,7 +17,6 @@ public class UserRouter {
     @Bean
     public RouterFunction<ServerResponse> route(UserHandler userHandler) {
         return RouterFunctions.route(
-                GET("/api/users/{id}").and(accept(APPLICATION_JSON)), userHandler::findById);
+                GET("/login/{id}").and(accept(APPLICATION_JSON)), userHandler::login);
     }
-
 }
